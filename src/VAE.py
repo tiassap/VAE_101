cf845aaa6e1f8b33879fea082938a725a5c03899
Tias Saputro <istiasa.saputro@gmail.com>
2022-09-26 20:50:18 +0900

    Added backbone codes and folders

---
+++ src/VAE.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn as nn
+
+class VAE(nn.Module):
+    def __init__(self, encoder, decoder):
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def reparametrization(self):
+        pass
+
+    def load_data(self):
+        pass
+
+    def encode(self):
+        pass
+
+    def decode(self):
+        pass
+
+    def train(self):
+        pass
+
+    def calc_loss(self):
+        pass
+
+    def save_model(self):
+        pass
+
+    def plot_score(self):
+        pass
+
+class Encoder(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
+
+class Decoder(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
---
+++ src/model.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def mlp_1():
+    pass
+
+def conv_1():
+    pass
